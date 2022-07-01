98b8888b5416aa47e76378e8c946491017f1e263
igpdev <iguerrero.dev@gmail.com>
2022-07-01 18:25:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-sxkycz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-sxkycz)
---
+++ src/Child.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button @click="emitFetchUsersEvent">Child Button</button>
+    <span v-if="loading">fetching users...</span>
+    <br></br>
+    <span v-if="users">
+    <!-- EXERCISE 02: ITERATE OVER ITEMS-->
+    <!-- ANSWER -->
+    <li>Got the users</li>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data() {
+    return {
+      loading: false,
+      users: null,
+    };
+  },
+  methods: {
+
+    emitFetchUsersEvent() {
+      this.loading = true;
+      this.users = null;
+      
+      this.$emit
+      ('fetchUsers', (res) => {
+        this.loading = false;
+        this.users = res;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+div {
+  display: grid;
+  place-items: center;
+}
+</style>
